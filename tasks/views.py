7f105baec4f8cf6f9385168fe98f32b8687acf8d
Hamnah <hamnah.1.rassen@kcl.ac.uk>
2023-11-14 15:13:15 +0000

    Fix requests table errors

--- tasks/views.py
+++ tasks/views.py
@@ -27,9 +27,9 @@ def home(request):
 
     return render(request, 'home.html')
 
-# def requests_table(request):
-#     invites = get_invites()
-#     return render(request, 'dashboard_html', {'invites': invites})
+def requests_table(request):
+    invites = get_invites()
+    return render(request, 'dashboard_html', {'invites': invites})
 
 # def fake_dashboard(request):
 #     fake_invite = Invite(sender='TestSender', message='TestMessage')
