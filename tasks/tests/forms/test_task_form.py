b7be74e8655d93033a57b49e9f0418d73491040b
Jacob Whiting <jacob.whiting@kcl.ac.uk>
2023-11-28 10:40:52 +0000

    Fix task form tests

--- tasks/tests/forms/test_task_form.py
+++ tasks/tests/forms/test_task_form.py
@@ -1,5 +1,6 @@
 """Unit tests of the task form."""
 from django import forms
+from datetime import datetime, timedelta
 from django.test import TestCase
 from tasks.forms import TaskForm
 from tasks.models import Task, User, Team, Team_Members
@@ -17,7 +18,8 @@ class TaskFormTestCase(TestCase):
         self.form_input = {
             'title' : 'Test task',
             'description' : 'This is a test task',
-            'assign_to_user' : [self.team_member.username.id],
+            'assigned_to' : [self.team_member.username.id],
+            'due_date' : (datetime.now().date() + timedelta(days=1)),
         }
 
         self.team = Team.objects.create(team_id = 1, 
@@ -32,8 +34,8 @@ class TaskFormTestCase(TestCase):
         self.assertIn('description', form.fields)
         description_field = form.fields['description']
         self.assertTrue(isinstance(description_field.widget, forms.Textarea))
-        self.assertIn('assign_to_user', form.fields)
-        assigned_to_field = form.fields['assign_to_user']
+        self.assertIn('assigned_to', form.fields)
+        assigned_to_field = form.fields['assigned_to']
         self.assertTrue(isinstance(assigned_to_field, forms.ChoiceField))
 
     def test_valid_task_form(self):
@@ -51,7 +53,8 @@ class TaskFormTestCase(TestCase):
             title = "Task title",
             description = "Description of the task",
             created_by = self.user,
-            related_to_team = self.team
+            related_to_team = self.team,
+            due_date = (datetime.now().date() + timedelta(days=2))
         )
         task.assigned_to.set([self.team_member])
 
