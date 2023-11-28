a9871bce3073d82b9bdbe73132d9b0f3694a06bd
Jacob Whiting <jacob.whiting@kcl.ac.uk>
2023-11-28 10:54:18 +0000

    Add test for due_date in task_form tests

--- tasks/tests/forms/test_task_form.py
+++ tasks/tests/forms/test_task_form.py
@@ -9,7 +9,8 @@ class TaskFormTestCase(TestCase):
     """Unit tests of the task form."""
 
     fixtures = [
-        'tasks/tests/fixtures/default_user.json'
+        'tasks/tests/fixtures/default_user.json',
+        'tasks/tests/fixtures/other_users.json'
     ]
 
     def setUp(self):
@@ -37,22 +38,25 @@ class TaskFormTestCase(TestCase):
         self.assertIn('assigned_to', form.fields)
         assigned_to_field = form.fields['assigned_to']
         self.assertTrue(isinstance(assigned_to_field, forms.ChoiceField))
+        self.assertIn('due_date', form.fields)
+        due_date_field = form.fields['due_date']
+        self.assertTrue(isinstance(due_date_field.widget, forms.DateInput))
 
     def test_valid_task_form(self):
         form = TaskForm(team_id = 1, data=self.form_input)
         self.assertTrue(form.is_valid())
 
     def test_invalid_task_form(self):
-        input = {'title': 'x'*101 }
-        form = TaskForm(team_id = 1, data=input)
+        self.form_input['title'] = 'x'*101 
+        form = TaskForm(team_id = 1, data=self.form_input)
         self.assertFalse(form.is_valid())
 
-
     def test_form_must_save_correctly(self):
+        testUser = User.objects.get(username='@janedoe')
         task = Task.objects.create(
             title = "Task title",
             description = "Description of the task",
-            created_by = self.user,
+            created_by = testUser,
             related_to_team = self.team,
             due_date = (datetime.now().date() + timedelta(days=2))
         )
@@ -65,5 +69,17 @@ class TaskFormTestCase(TestCase):
         self.assertEqual(after_count, before_count)
         self.assertEqual(task.title, 'Test task')
         self.assertEqual(task.description, 'This is a test task')
-        self.assertEqual(task.created_by, self.user)
+        self.assertEqual(task.created_by, testUser)
         self.assertQuerysetEqual(task.assigned_to.all(), [repr(self.team_member)], transform=repr)
+        self.assertEqual(task.related_to_team, self.team)
+        self.assertEqual(task.due_date, (datetime.now().date() + timedelta(days=1)))
+
+    def test_due_date_must_be_in_the_future(self):
+        self.form_input['due_date'] =  (datetime.now().date() - timedelta(days=1))
+        form = TaskForm(team_id = 1, data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_must_be_assigned_to_a_user(self):
+        self.form_input['assigned_to'] = []
+        form = TaskForm(team_id = 1, data=self.form_input)
+        self.assertFalse(form.is_valid())
